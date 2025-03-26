941938aa96a0b9722e0a4eec5a9d82b3576da3fa
Sherry <vythao219@gmail.com>
2025-03-26 12:33:07 +0700

    change path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // publicPath: process.env.NODE_ENV === 'production'
-    //   ? '/vythaor-portfolio/'
-    //   : '/'
-      publicPath: '/'
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/vythaor-portfolio/'
+      : '/'
+      // publicPath: '/'
   }
